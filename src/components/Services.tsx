5824a4169f3366cd01198821da1a14b82d86f80a
Moiz Qureshi <147783174+Moizqureshi2002@users.noreply.github.com>
2025-05-19 07:47:10 +0000

    Changed the animations in the Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -16,14 +16,14 @@ const Services: React.FC = () => {
       id: 1,
       title: "Job Postings",
       animationUrl:
-        "https://lottie.host/5e343f28-acc6-421a-b29a-604a0d75bed8/987UBsYwAw.lottie",
+        "https://lottie.host/fac14f9b-e374-491d-b1a8-930ec3b216bc/FcQJtV0P89.lottie",
       description: "I help improve your website’s visibility on search engines.",
     },
     {
       id: 2,
       title: "Web Development",
       animationUrl:
-        "https://lottie.host/5ad23d75-3ea5-4ff6-8d7d-73f68fb62d33/4vf8hJSGa7.lottie",
+        "https://lottie.host/5e343f28-acc6-421a-b29a-604a0d75bed8/987UBsYwAw.lottie",
       description: "I build responsive and user-friendly websites.",
     },
     {
@@ -44,14 +44,14 @@ const Services: React.FC = () => {
       id: 5,
       title: "Content Writing",
       animationUrl:
-        "https://lottie.host/5e343f28-acc6-421a-b29a-604a0d75bed8/987UBsYwAw.lottie",
+        "https://lottie.host/d8910849-1242-4ab8-bfc7-839295c33fd7/hu6UKmclbp.lottie",
       description: "I help improve your website’s visibility on search engines.",
     },
     {
       id: 6,
       title: "Email Marketing",
       animationUrl:
-        "https://lottie.host/5e343f28-acc6-421a-b29a-604a0d75bed8/987UBsYwAw.lottie",
+        "https://lottie.host/62691aad-be70-40cd-afd3-f5051a3d3164/9fAczqaJCx.lottie",
       description: "I help improve your website’s visibility on search engines.",
     },
   ];
@@ -68,7 +68,7 @@ const Services: React.FC = () => {
   };
 
   return (
-    <section className="p-4 relative">
+    <section className="p-4  relative">
       <h2 className="text-2xl font-bold mb-4 text-center">My Services</h2>
 
       <div className="flex items-center">
@@ -90,7 +90,7 @@ const Services: React.FC = () => {
               key={id}
               className="min-w-[250px] bg-white p-4 rounded-lg shadow hover:shadow-lg flex-shrink-0"
             >
-              <div className="h-32 mb-2">
+              <div className="h-32  mb-2">
                 <DotLottieReact src={animationUrl} loop autoplay />
               </div>
               <h3 className="text-lg font-semibold text-center">{title}</h3>
